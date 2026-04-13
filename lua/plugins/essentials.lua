7528f0a513eda53fbffa9c8597c0c26feafcc197
damonpeng <damonpeng0308@gmail.com>
2026-04-13 11:08:44 +0800

    修复nvim报错问题

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -3,7 +3,7 @@ return {
     {'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
-            require('nvim-treesitter.configs').setup({
+            require('nvim-treesitter').setup({
                 ensure_installed = { "lua", "go", "cpp", "c", "markdown", "markdown_inline" },
                 highlight = { enable = true },
                 indent = { enable = true },
